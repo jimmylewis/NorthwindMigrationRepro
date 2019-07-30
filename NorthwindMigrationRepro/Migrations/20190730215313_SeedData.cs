eb5dd2417e8568b3672ec20097fd14f3c3d58f4c
Jimmy Lewis <jimmy.lewis@microsoft.com>
2019-07-30 15:09:17 -0700

    Add seed data script and migration

---
+++ NorthwindMigrationRepro/Migrations/20190730215313_SeedData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NorthwindMigrationRepro.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(DbSeedData.SqlQuery);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
